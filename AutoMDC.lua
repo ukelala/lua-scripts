962c5c43eae442933ce13cc057a817c672086d23
ukelala <137701984+ukelala@users.noreply.github.com>
2023-07-02 01:01:03 +0200

    Add files via upload

--- AutoMDC.lua
+++ AutoMDC.lua
@@ -11,25 +11,38 @@ sampAddChatMessage("[AUTOMDC] WORKING!", 0xffba74)
             if not sampIsChatInputActive() then
 				wait(800)
                 sampSendChat("/mdc " .. mdc)
-				wait(2000)
+				wait(800)
 				nick = sampGetPlayerNickname(mdc)
-				if pri == " Неизвестно" then
-				sampSendChat("/f [PO-II]: Имя: " .. nick .. ". Уровень розыска: " .. zv)
-				else
-				sampSendChat("/f [PO-II]: Имя: " .. nick .. ". Уровень розыска: " .. zv .. ". Причина:" .. pri)
+				if zv ~= nil then
+						if pri == " Неизвестно" then
+						sampSendChat("/f [PO-II]: Имя: " .. nick .. ". Уровень розыска: " .. zv)
+						else
+						sampSendChat("/f [PO-II]: Имя: " .. nick .. ". Уровень розыска: " .. zv .. ". Причина:" .. pri)
+						end
+					mdc=nil
+					zv=nil
+					pri=nil
+				else 
+				sampAddChatMessage("[AUTOMDC] Сядь в тачку или войди в подходящий интерьер!", 0xffba74)
 				end
 				mdc=nil
-            end
+				zv=nil
+				pri=nil
+            end	
         end
     end
 end
 
 function sampev.onServerMessage(color, text)
-	if string.find(text,"10-56",1,true) then
+	if string.find(text,"10-56",1,true) and color == -1920073729 then
 		_,i = string.find(text,"10-56",1,true)
 		i=i+1
 		mdc = string.match(text, "%d+",i)
-		sampAddChatMessage("[AUTOMDC] processing id " .. mdc, 0xffba74)
+		if mdc ~= nil then
+		sampAddChatMessage("[AUTOMDC] Processing id " .. mdc, 0xffba74)
+		else
+		sampAddChatMessage("[AUTOMDC] No id to process!", 0xffba74)
+		end
 	end
 	
 	if string.find(text,"Уровень розыска:",1,true) then
@@ -39,4 +52,5 @@ function sampev.onServerMessage(color, text)
 	if string.find(text,"Причина:",1,true) then
 		pri = string.gsub(text, "Причина: ","")
 	end
+	
 end
